ce2a1da48f9c9fbaae3435506b9f5d77afe76f52
glitjch <t.jamesphan@gmail.com>
2022-01-11 03:53:21 +0000

    compass assignment head

---
+++ head.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  return (actual === expected)
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
