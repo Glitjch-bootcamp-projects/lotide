b03ebac61c45382e1b36737d3da5997bc5f5071f
glitjch <t.jamesphan@gmail.com>
2022-01-12 07:11:42 +0000

    fixed assertArraysEqual naming and submitting without.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,11 @@
-const assertArrayEquals = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   let maxLength = Math.max(arr1.length, arr2.length);
   for (let i = 0; i < maxLength; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`☐💔💔💔☐ Assertion Failed: ${arr1} !== ${arr2}`);
+      return console.log(`☐💔💔💔☐ Assertion Failed: ${arr1} !== ${arr2}`);
     }
   }
-  console.log(`☐💚💚💚☐ Assertion Passed: ${arr1} === ${arr2}`);
+  return console.log(`☐💚💚💚☐ Assertion Passed: ${arr1} === ${arr2}`);
 };
-assertArrayEquals([5, 6, 7], [5, 6, 7]);
+
+assertArraysEqual([5, 6, 7], [5, 6, 7]);
