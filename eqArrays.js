d6213625737d035146ea96d886e9bc52621be107
glitjch <t.jamesphan@gmail.com>
2022-01-12 06:01:40 +0000

    compass assignment eqArray correction

--- eqArrays.js
+++ eqArrays.js
@@ -5,10 +5,14 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  let checkEqArr = "";
-  arr1.join("") === arr2.join("") ? checkEqArr = true : checkEqArr = false;
-  return checkEqArr;
+  let maxLength = Math.max(arr1.length, arr2.length);
+  for (let i = 0; i < maxLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    } 
+  }
+  return true;
 };
 
-// Am I doing something wrong? This solution, converting entire arrays into strings, seems too good to be true...
+//Am I doing something wrong? This solution, converting entire arrays into strings, seems too good to be true...
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
