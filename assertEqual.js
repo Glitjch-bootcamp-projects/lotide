8863cf668513475a49be66f04e22cd2f6e787062
glitjch <t.jamesphan@gmail.com>
2022-01-13 05:29:15 +0000

    W1D3 eqObjects and eslint on eqArrays

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+//WARNING: assertEqual can really only be used for checking primitive return types.
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   return (actual === expected)
--- eqArrays.js
+++ eqArrays.js
@@ -8,8 +8,8 @@ const eqArrays = function(arr1, arr2) {
   let maxLength = Math.max(arr1.length, arr2.length);
   for (let i = 0; i < maxLength; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
-    } 
+      return false;
+    }
   }
   return true;
 };
