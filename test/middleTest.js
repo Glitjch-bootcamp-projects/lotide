767cd68fc45f3c7955a6f7d7f012244896dcb700
glitjch <t.jamesphan@gmail.com>
2022-01-17 23:41:09 +0000

    mocha and chai middleTest.js and headTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,35 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-// Test
-middle([5, 6, 7, 8]);
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([2, 3, 4]); // => [3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
 
-assertArraysEqual(middle([5, 6, 7, 8]), [6, 7]);
+describe("#middle", () => {
+  
+  it("returns 6 and 7 for [5, 6, 7, 8]", () => {
+    const input = [5, 6, 7, 8];
+    const expected = [6, 7];
+    assert.deepEqual(middle(input), expected);
+  });
+
+  it("returns [] for ['1']", () => {
+    const input = ['1'];
+    const expected = [];
+    assert.deepEqual(middle(input), expected);
+  });
+  
+  it("returns [3] for [2, 3, 4]", () => {
+    const input = [2, 3, 4];
+    const expected = [3];
+    assert.deepEqual(middle(input), expected);
+  });
+  
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    const input = [1, 2, 3, 4, 5, 6];
+    const expected = [3, 4];
+    assert.deepEqual(middle(input), expected);
+  });
+
+});
+
+
+// const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
