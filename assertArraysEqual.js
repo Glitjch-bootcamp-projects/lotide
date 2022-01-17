9205b0edc9d28f6aa77a436f404448dfe4b72575
glitjch <t.jamesphan@gmail.com>
2022-01-17 18:03:51 +0000

    update module refactoring on assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,10 @@
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+
 const assertArraysEqual = function(arr1, arr2) {
-  let maxLength = Math.max(arr1.length, arr2.length);
-  for (let i = 0; i < maxLength; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return console.log(`☐💔💔💔☐ Assertion Failed: ${arr1} !== ${arr2}`);
-    }
-  }
-  return console.log(`☐💚💚💚☐ Assertion Passed: ${arr1} === ${arr2}`);
+  assertEqual(eqArrays(arr1, arr2), true);
 };
 
-assertArraysEqual([5, 6, 7], [5, 6, 7]);
+
+module.exports = assertArraysEqual;
+
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// Test
+assertArraysEqual([5, 6, 7], [5, 6, 7]);
