5c14c306c6e4dd4cc16b6fca6471514b76d1b4d0
glitjch <t.jamesphan@gmail.com>
2022-01-17 18:21:52 +0000

    updating test for assertEqual

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,3 +1,4 @@
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
